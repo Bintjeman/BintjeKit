2ae0e19af8796533fc782d8ae9bbc379db2dc63e
bintjeman <bintjeman@eratigena.org>
2025-06-14 12:36:07 +0200

    Extend minimal example with custom `IEngine`, `IEngineRenderer`, and `IRenderer` implementations. Update Core initialization accordingly.

--- examples/minimal_example/src/main.cpp
+++ examples/minimal_example/src/main.cpp
@@ -7,9 +7,25 @@
 */
 #include <iostream>
 #include <bintjekit/core/core.hpp>
-#include "bintjekit/core/core_builder.hpp"
+#include <bintjekit/core/core_builder.hpp>
+#include <bintjekit/renderer/i_renderer.hpp>
+#include <bintjekit/renderer/i_engine_renderer.hpp>
+#include <bintjekit/engine/i_engine.hpp>
+class MyEngine : public bnjkit::engine::IEngine {
+public:
+    void update() override{}
+};
+class MyEngineRenderer : public bnjkit::renderer::IEngineRenderer {};
+class MyRenderer: public bnjkit::renderer::IRenderer {
+public:
+    void on_sfml_event(const sf::Event &event) override{}
+};
 int main() {
     std::cout << "Hello World!" << std::endl;
-    auto core = bnjkit::core::CoreBuilder().build();
+    auto core = bnjkit::core::CoreBuilder()
+    .set<MyEngine>()
+    .set<MyEngineRenderer>()
+    .set<MyRenderer>()
+    .build();
     core->run();
 }
