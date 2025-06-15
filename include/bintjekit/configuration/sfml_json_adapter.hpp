281bcfeb15abf6b5bccefd7de7bb5bfdd7d5b61a
bintjeman <bintjeman@eratigena.org>
2025-06-15 15:56:08 +0200

    Refactor SFML-JSON adapter: replace specialized `to_json`/`from_json` implementations with templates for `sf::Vector2` and `sf::Rect`. Streamline `sf::Color` handling.

--- include/bintjekit/configuration/sfml_json_adapter.hpp
+++ include/bintjekit/configuration/sfml_json_adapter.hpp
@@ -12,12 +12,32 @@
 
 namespace bnjkit {
     namespace core {
-        void to_json(nlohmann::json &j, const sf::Vector2f &v);
-        void from_json(const nlohmann::json &j, sf::Vector2f &v);
+        template<typename T>
+        void to_json(nlohmann::json &j, const sf::Vector2<T> &v) {
+            j = nlohmann::json{{"x", v.x}, {"y", v.y}};
+        }
+        template<typename T>
+        sf::Vector2<T> from_json(const nlohmann::json &j) {
+            return {j["x"], j["y"]};
+        }
         void to_json(nlohmann::json &j, const sf::Color &c);
-        void from_json(const nlohmann::json &j, sf::Color &c);
-        void to_json(nlohmann::json &j, const sf::FloatRect &r);
-        void from_json(const nlohmann::json &j, sf::FloatRect &r);
+        sf::Color from_json(const nlohmann::json &j);
+        template<typename T>
+        void to_json(nlohmann::json &j, const sf::Rect<T> &r) {
+            j = nlohmann::json{
+                    {"position", {{"x", r.position.x}, {"y", r.position.y}}},
+                    {"size", {{"x", r.size.x}, {"y", r.size.y}}}
+            };
+        }
+        template<typename T>
+        sf::Rect<T> from_json(const nlohmann::json &j) {
+            sf::Rect<T> r;
+            r.position.x = j["position"]["x"].get<T>();
+            r.position.y = j["position"]["y"].get<T>();
+            r.size.x = j["size"]["x"].get<T>();
+            r.size.y = j["size"]["y"].get<T>();
+            return r;
+        }
     } // core
 } // bnjkit
 #endif //SFML_JSON_ADAPTER_HPP
--- src/configuration/sfml_json_adapter.cpp
+++ src/configuration/sfml_json_adapter.cpp
@@ -5,39 +5,19 @@
  */
 #include "bintjekit/configuration/sfml_json_adapter.hpp"
 
-namespace bnjkit::core::conf {
-    void to_json(nlohmann::json &j, const sf::Vector2f &v) {
-        j = nlohmann::json{{"x", v.x}, {"y", v.y}};
-    }
+namespace bnjkit {
+    namespace conf {
+        void to_json(nlohmann::json &j, const sf::Color &c) {
+            j = nlohmann::json{{"r", c.r}, {"g", c.g}, {"b", c.b}, {"a", c.a}};
+        }
 
-    void from_json(const nlohmann::json &j, sf::Vector2f &v) {
-        v.x = j.value("x", 0.0f);
-        v.y = j.value("y", 0.0f);
-    }
-
-    void to_json(nlohmann::json &j, const sf::Color &c) {
-        j = nlohmann::json{{"r", c.r}, {"g", c.g}, {"b", c.b}, {"a", c.a}};
-    }
-
-    void from_json(const nlohmann::json &j, sf::Color &c) {
-        c.r = j.value("r", 255);
-        c.g = j.value("g", 255);
-        c.b = j.value("b", 255);
-        c.a = j.value("a", 255);
-    }
-
-    void to_json(nlohmann::json &j, const sf::FloatRect &r) {
-        j = nlohmann::json{
-            {"left", r.left}, {"top", r.top},
-            {"width", r.width}, {"height", r.height}
-        };
-    }
-
-    void from_json(const nlohmann::json &j, sf::FloatRect &r) {
-        r.left = j.value("left", 0.0f);
-        r.top = j.value("top", 0.0f);
-        r.width = j.value("width", 0.0f);
-        r.height = j.value("height", 0.0f);
-    }
-}
-#endif //SFML_JSON_ADAPTER_HPP
+        sf::Color from_json(const nlohmann::json &j) {
+            sf::Color c;
+            c.r = j.value("r", 255);
+            c.g = j.value("g", 255);
+            c.b = j.value("b", 255);
+            c.a = j.value("a", 255);
+            return c;
+        }
+    } // conf
+} // bnjkit
