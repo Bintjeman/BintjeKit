fdcb4f17ba0149b8aa86aa975dd28679b4cb13ca
bintjeman <bintjeman@eratigena.org>
2025-06-24 21:43:26 +0200

    Add `fmt` formatter for `sf::Color`
    - Introduced a custom `fmt` formatter for `sf::Color` to enable formatted output.
    - Supports both RGB and RGBA representations depending on alpha value.

--- common/include/fmt_sfml/fmt_sfml.hpp
+++ common/include/fmt_sfml/fmt_sfml.hpp
@@ -7,10 +7,23 @@
 #define BNJKIT_COMMONFMT_SFML_HPPFMT_SFML_HPP
 #pragma once
 #include <SFML/Graphics.hpp>
-#if SPDLOG_USE_STD_FORMAT
-#include <format>
-#else
+#include <SFML/Graphics/Color.hpp>
 #include <spdlog/fmt/fmt.h>
+template<>
+struct fmt::formatter<sf::Color> {
+    constexpr auto parse(format_parse_context& ctx) -> decltype(ctx.begin()) {
+        return ctx.begin();
+    }
+
+    template<typename FormatContext>
+    auto format(const sf::Color& clr, FormatContext& ctx) -> decltype(ctx.out()) {
+        if (clr.a != 255) {
+            return fmt::format_to(ctx.out(), "({}, {}, {}, {})", clr.r, clr.g, clr.b, clr.a);
+        } else {
+            return fmt::format_to(ctx.out(), "({}, {}, {})", clr.r, clr.g, clr.b);
+        }
+    }
+};
 template<typename T>
 struct fmt::formatter<sf::Vector2<T> > {
     constexpr auto parse(format_parse_context& ctx) -> decltype(ctx.begin()) {
@@ -35,5 +48,4 @@ struct fmt::formatter<sf::Rect<T> > {
                               rect.position.x, rect.position.y, rect.size.x, rect.size.y);
     }
 };
-#endif
 #endif // BNJKIT_COMMONFMT_SFML_HPPFMT_SFML_HPP
