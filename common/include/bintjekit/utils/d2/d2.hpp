6822ac312214fab739c7d371a80d09111321c517
bintjeman <bintjeman@eratigena.org>
2025-07-04 11:05:11 +0200

    Add `PIX2` constant to `d2` utility module for improved calculation clarity
    - Introduced `PIX2` as a constexpr for `2 * π` in `d2.hpp`.
    - Included `<cmath>` for mathematical constants.

--- common/include/bintjekit/utils/d2/d2.hpp
+++ common/include/bintjekit/utils/d2/d2.hpp
@@ -6,11 +6,12 @@
 #ifndef BINTJEKIT_D2_HPP
 #define BINTJEKIT_D2_HPP
 #pragma once
+#include <cmath>
 #include <SFML/System/Vector2.hpp>
+constexpr float PIX2 = 2 * M_PIf;
 
 namespace bnjkit::utils::d2 {
     sf::Vector2f rad_to_vec(float rad);
-
     float vec_to_rad(sf::Vector2f vec);
 }
 #endif // BINTJEKIT_D2_HPP
